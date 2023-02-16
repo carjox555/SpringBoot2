b2d5506533b6f791cd7e1ad26559564abe64c98a
CARJOX <carjox2021@gmail.com>
2023-02-15 21:32:16 -0500

    inicio

---
+++ src/main/java/com/spring2/demo/controllers/HiWorld.java
@@ -0,0 +1,14 @@
+package com.spring2.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiWorld {
+
+    @GetMapping("/saludo/{name}")
+    public String saludo(@PathVariable String name){
+        return "hola sss " + name;
+    }
+}
