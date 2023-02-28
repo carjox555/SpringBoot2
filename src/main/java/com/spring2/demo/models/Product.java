9ba4571b58fdf0b26395470eae608b2010ad3ad4
carjox <carlosbm1982@yahoo.es>
2023-02-28 14:18:39 -0500

    oficina2802-1

--- src/main/java/com/spring2/demo/models/Product.java
+++ src/main/java/com/spring2/demo/models/Product.java
@@ -1,14 +1,22 @@
 package com.spring2.demo.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
 
 import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table
+
 public class Product {
+    @Id
     private Long id;
     private String name;
     private float price;
